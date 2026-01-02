12959e81b77f2535bce2deb5a6aec8ed06d72b6b
joshuali3110 <joshuali3110@ucla.edu>
2026-01-01 23:36:40 -0800

    leetcode 3186

---
+++ leetcode/regular/medium/3186/solution2.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/maximum-total-damage-with-spell-casting/submissions/
+
+# revised solution, still makes intuitive sense, much cleaner (and faster)
+
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        counts = Counter(power)
+        unique_sorted = sorted(counts.keys())
+
+        prev1, prev2, prev3 = (0, 0), (0, 0), (0, 0)
+
+        for p in unique_sorted:
+            safe = -1
+
+            if prev1[1] < p - 2:
+                safe = prev1[0]
+            elif prev2[1] < p - 2:
+                safe = prev2[0]
+            else:
+                safe = prev3[0]
+            
+            nxt = (max(prev1[0], safe + p * counts[p]), p)
+
+            prev3 = prev2
+            prev2 = prev1
+            prev1 = nxt
+        
+        return max(prev1)
+
